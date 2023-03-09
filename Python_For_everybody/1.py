a0b2e2204defec88ebd33112a0156542999bda1d
Thiago G. Alencar <alencar.g.thiago@gmail.com>
2023-03-09 15:17:46 -0300

    primeiros exercícios adicionados

---
+++ Python_For_everybody/1.py
@@ -0,0 +1,4 @@
+#Escreva um programa que use inputs para solicitar ao
+#usuário seu nome e, em seguida, faça um cumprimento.
+name = input("Digite seu nome")
+print(" Seja muito bem vindo:", name)
---
+++ cap2_datascience_do_zero/lambda.py
@@ -0,0 +1,13 @@
+#aprendemos manipular strings da seguinte forma
+# name="maria"
+# idade=30
+
+# mensagem= "Meu nome é", name," e eu tenho", idade, "anos de idade"
+# print(mensagem)
+
+#forma mais efetiav
+name= "maria"
+idade=15
+
+mensagem = f"Meu nome é {name} e eu tenho {idade} anos de idade"
+print(mensagem)
